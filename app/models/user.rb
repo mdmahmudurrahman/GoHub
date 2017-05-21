9d8a001133c7ebc72f9c1dfe239a10532094d0cc
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2017-05-21 19:23:55 +0600

    refs1_signup_signin_ui_and_functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :orders
   has_many :wish_lists
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :omniauthable,
+    :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "static_pages#search_gear"
 
   get "about", to: "static_pages#about"
