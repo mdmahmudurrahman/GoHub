530bd861e563c169d243c4b0627fbc75b22ea61f
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2017-05-27 10:17:10 +0600

    sending email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,30 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  # config.action_mailer.delivery_method = :smtp
+  # # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :user_name            => ENV['gmail_username'],
+  #   :password             => ENV['gmail_password'],
+  #   :authentication       => "plain",
+  #   :enable_starttls_auto => true
+  # }
+
+  config.action_mailer.default_url_options = {host: "go-hub.herokuapp.com"}
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["gohub_domain"],
+    user_name: ENV["gohub_user_name"],
+    password: ENV["gohub_pass"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
+
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
